cd2ff5f06c13c3c2f58575330959e8a359f87a7c
Tugino <nguyendinhtu.job@gmail.com>
2023-01-21 15:38:18 +0700

    mogo

---
+++ public/scripts/main.js
@@ -0,0 +1,23 @@
+const menuToggle = document.querySelector(".header-toggle");
+const menuHeader = document.querySelector(".header-menu");
+const expandClass = "is-expand";
+menuToggle.addEventListener("click", () => {
+  menuHeader.classList.add(expandClass);
+});
+window.addEventListener("click", e => {
+  if (!menuHeader.contains(e.target) && !e.target.matches(".header-toggle")) {
+    menuHeader.classList.remove(expandClass);
+  }
+});
+$(document).ready(function () {
+  $(".quotes-container").slick({
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fal fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fal fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [{
+      breakpoint: 767,
+      settings: {
+        arrows: false
+      }
+    }]
+  });
+});
